376f8d797627525e408d7882c58ce15835816845
maruf10155 <maruf10155@users.noreply.github.com>
2017-07-02 17:53:01 -0400

    Update plot3.r

--- plot3.r
+++ plot3.r
@@ -1,15 +1,17 @@
+# loading packages
 library(lubridate)
 library(data.table)
 library(dplyr)
-a=fread("ExData_Plotting1/household_power_consumption.txt",na.strings = c("?"))
-a1=filter(a,dmy(Date)=="2007-02-01"|dmy(Date)=="2007-02-02")
-setwd("ExData_Plotting1")
+a=fread("ExData_Plotting1/household_power_consumption.txt",na.strings = c("?")) # loading dataset
+a1=filter(a,dmy(Date)=="2007-02-01"|dmy(Date)=="2007-02-02") # subsetting required dataset
+setwd("ExData_Plotting1") # set working directory
 a2=dmy(a1$Date)+hms(a1$Time)
-a1$timedate=a2
+a1$timedate=a2 # creating a column 
+# combine three plots in one plot
 with(a1,plot(timedate,Sub_metering_1,ylab="Energy sub metering",type="s"))
 with(a1,lines(timedate, Sub_metering_2,col="red"))
 with(a1,lines(timedate, Sub_metering_3,col="blue"))
 legend("topright",col=c("black","red","blue"),lty="solid",
-       legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
-dev.copy(png, width=480, height=480, file = "plot3.png")
-dev.off()
+       legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3")) # create legend
+dev.copy(png, width=480, height=480, file = "plot3.png") # copy to a device
+dev.off() # closing device
