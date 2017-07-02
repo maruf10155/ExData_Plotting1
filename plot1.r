4466dfa95510a7bb21e272ee0eee832044cd1df5
maruf10155 <maruf10155@users.noreply.github.com>
2017-07-02 17:46:48 -0400

    Update plot1.r

--- plot1.r
+++ plot1.r
@@ -1,9 +1,11 @@
-library(lubridate)
+# loading packages
+library(lubridate) 
 library(data.table)
 library(dplyr)
-a=fread("ExData_Plotting1/household_power_consumption.txt",na.strings = c("?"))
-a1=filter(a,dmy(Date)=="2007-02-01"|dmy(Date)=="2007-02-02")
-setwd("ExData_Plotting1")
-png(file="plot1.png",width =480,height = 480 )
+a=fread("ExData_Plotting1/household_power_consumption.txt",na.strings = c("?")) # load the dataset
+a1=filter(a,dmy(Date)=="2007-02-01"|dmy(Date)=="2007-02-02") # subsetting the required dataset
+setwd("ExData_Plotting1") # set working directory
+# plotting the histogram
+png(file="plot1.png",width =480,height = 480 ) 
 hist(a1$Global_active_power,col = "red",main = "Global Active Power", xlab = "Golbal Active Power (killowatts)",ylab = "Frequency")
-dev.off()
+dev.off() # closing the device
